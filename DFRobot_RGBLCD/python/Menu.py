16e7e84de7d7edf59046624abb61414f64590b01
BasVDriel <basvandriel1999@hotmail.nl>
2022-05-31 22:04:01 +0200

    what I said before

--- DFRobot_RGBLCD/python/Menu.py
+++ DFRobot_RGBLCD/python/Menu.py
@@ -2,23 +2,18 @@ import sys
 sys.path.append('.')
 import RPi.GPIO as GPIO
 import rgb1602
-import time
+from Printbox import *
 lcd = rgb1602.RGB1602(16,2)
 
 class Menu:
-  def __init__(self, name, state):
-    self.name = name
-    self.state = state
+  def __init__(self):
+    self.printBoxes = []
+
+  def addPrintbox(self, pb):
+    self.printBoxes.append(pb)
 
   def display(self):
-    if len(self.name) <= 16:    #print menu name
-        lcd.setCursor(0,0)
-        lcd.printout(self.name)
-    if self.state == 0:
-        lcd.setCursor(0,1)
-        lcd.printout("Failed!")
-        lcd.setRGB(255,0,0)
-    if self.state == 1:
-        lcd.setCursor(0,1)
-        lcd.printout("Running!")
-        lcd.setRGB(0,255,0)
+    for pb in self.printBoxes:
+      pb.display()
+
+
---
+++ DFRobot_RGBLCD/python/Printbox.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append('.')
+import RPi.GPIO as GPIO
+import rgb1602
+lcd = rgb1602.RGB1602(16,2)
+
+class Printbox:
+    def __init__(self, line, begin, end):
+        if begin > 15 or end > 15:
+            print("box out of bounds")
+            return
+        if begin < 0 or end < 0:
+            print("box out of bounds")
+            return
+        if line < 0 or line > 1:
+            print("line index can only be 0 or 1")
+            return
+        self.line = line
+        self.begin = begin
+        self.end = end
+        self.message = ""
+        self.empty = "                "
+
+    def setMessage(self, message):
+        if not isinstance(self.message, str):
+            print("message not a string")
+            return
+        self.message = message + self.empty
+
+    def display(self):
+        lcd.setCursor(self.begin, self.line)
+        lcd.printout(self.message[0:self.end+1])
+    
